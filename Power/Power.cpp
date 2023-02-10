1dacbb9b1443c5507786e0bf3cb2b533c90d1ebe
Sadman Rafith <108008599+SadmanRafith@users.noreply.github.com>
2023-02-10 11:58:24 +0600

    Update Power.cpp

--- Power/Power.cpp
+++ Power/Power.cpp
@@ -11,17 +11,5 @@ int main()
     cin>> num1;
     square = pow(num,num1);
     cout<< square<<endl;
-    int digit[square],number;
-    //defining a loop for splitting the number
-    for(int i=0; i<sizeof square; i++)
-    {
-        digit[i]=number%10;
-        number=number/10;
-    }
-    //for reading the digits stored in digits array
-    cout <<"digits are"<<endl;
-    for(int j=sizeof square-1 ; j>=0 ; j-- )
-    {
-        cout<<digit[j]<<", ";
-    }
+    
 }
