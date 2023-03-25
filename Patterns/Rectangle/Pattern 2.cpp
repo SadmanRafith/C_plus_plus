9e8dd34f056313f71cc4240e92c05801520bd8ed
Sadman Rafith <108008599+SadmanRafith@users.noreply.github.com>
2023-03-25 11:55:11 +0600

    Add files via upload

---
+++ Patterns/Rectangle/Pattern 2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    for(int i=1;i<=8;i++)
+    {
+        for(int j=1;j<=8;j++)
+        {
+            if(i==1||i==8||j==1|| j==8)
+                cout<< "* ";
+            else
+                cout<< "  ";
+        }
+        cout<< "\n";
+    }
+}
